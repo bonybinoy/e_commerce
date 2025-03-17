dcc61836d8721a4c5f92e6e46406fec377343c62
Bony_Binoy <bonybinoy1@gmail.com>
2025-03-17 18:19:28 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/screens/login_page.dart

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Luxecart',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
